3a65429a576e12c7fc3fbb03e9c1c14876fbdc62
Dan Fishgold <d.fishgold@gmail.com>
2021-07-17 00:21:40 +0300

    Add key prop for code lines

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 export default function Code({ code }: { code: string }) {
   const lines = code.split('\n')
@@ -6,10 +6,10 @@ export default function Code({ code }: { code: string }) {
   return (
     <div className='code-container'>
       {lines.map((line, index) => (
-        <>
+        <Fragment key={index}>
           <span className='line-number'>{index + 1}</span>
           <pre className='code-line'>{line}</pre>
-        </>
+        </Fragment>
       ))}
     </div>
   )
