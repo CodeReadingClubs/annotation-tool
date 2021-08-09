760ffcb641f22cdfe7e9293a4591b2b4407feba0
Dan Fishgold <d.fishgold@gmail.com>
2021-08-09 23:58:47 +0300

    Remove unused variables

--- src/reducer.ts
+++ src/reducer.ts
@@ -2,30 +2,6 @@ import { AnyAction, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { v4 as uuid } from 'uuid'
 import { Arrow, Marker, Point, Rect } from './types'
 
-const code = `function configFromInput(config) {
-  var input = config._i;
-  if (isUndefined(input)) {
-      config._d = new Date(hooks.now());
-  } else if (isDate(input)) {
-      config._d = new Date(input.valueOf());
-  } else if (typeof input === 'string') {
-      configFromString(config);
-  } else if (isArray(input)) {
-      config._a = map(input.slice(0), function (obj) {
-          return parseInt(obj, 10);
-      });
-      configFromArray(config);
-  } else if (isObject(input)) {
-      configFromObject(config);
-  } else if (isNumber(input)) {
-      // from milliseconds
-      config._d = new Date(input);
-  } else {
-      hooks.createFromInputFallback(config);
-  }
-}
-`
-
 export type State = {
   code: string | null
   currentSelection: Selection | null
