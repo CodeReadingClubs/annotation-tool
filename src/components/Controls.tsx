5c40169c422d77fa4ba346f68d7529ec4ecb98b9
Dan Fishgold <d.fishgold@gmail.com>
2021-08-10 13:43:12 +0300

    Add localStorage debugging buttons
    One to copy the persisted state to the clipboard and one to replace it
    with the clipboard content.

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useFilePath } from '../hooks/useFile'
 import { setShowStraightArrows } from '../reducer'
 import { useDispatch, useSelector } from '../store'
 import { redo, reset, undo, useCanUndoRedo } from '../undoable'
@@ -7,7 +8,6 @@ import Export from './Export'
 export default function Controls() {
   const dispatch = useDispatch()
   const showStraightArrows = useSelector((state) => state.showStraightArrows)
-  const { canUndo, canRedo } = useCanUndoRedo()
 
   return (
     <div className='annotation-controls'>
@@ -18,16 +18,56 @@ export default function Controls() {
         onChange={(e) => dispatch(setShowStraightArrows(e.target.checked))}
       />
       <label htmlFor='straight-arrows'>Use straight arrows</label>
-      <div>
-        <button disabled={!canUndo} onClick={() => dispatch(undo())}>
-          undo
-        </button>
-        <button disabled={!canRedo} onClick={() => dispatch(redo())}>
-          redo
-        </button>
-        <button onClick={() => dispatch(reset())}>clear</button>
-      </div>
+      <UndoManagement />
       <Export />
+      {import.meta.env.DEV && <PersistedStateDebugging />}
+    </div>
+  )
+}
+
+function UndoManagement() {
+  const dispatch = useDispatch()
+  const { canUndo, canRedo } = useCanUndoRedo()
+
+  return (
+    <div>
+      <button disabled={!canUndo} onClick={() => dispatch(undo())}>
+        undo
+      </button>
+      <button disabled={!canRedo} onClick={() => dispatch(redo())}>
+        redo
+      </button>
+      <button onClick={() => dispatch(reset())}>clear</button>
+    </div>
+  )
+}
+
+function PersistedStateDebugging() {
+  const filePath = useFilePath()
+  const paste = useCallback(() => {
+    const key = `persist:state:${filePath}`
+    navigator.clipboard
+      .readText()
+      .then((text) => localStorage.setItem(key, text))
+      .then(() => console.log('pasted'))
+  }, [])
+
+  const copy = useCallback(() => {
+    const key = `persist:state:${filePath}`
+    const localStorageValue = localStorage.getItem(key)
+    if (!localStorageValue) {
+      console.error(`Value missing in local storage`)
+      return
+    }
+    navigator.clipboard
+      .writeText(localStorageValue)
+      .then(() => console.log('copied'))
+  }, [])
+
+  return (
+    <div>
+      <button onClick={copy}>copy persisted state</button>
+      <button onClick={paste}>paste persisted state</button>
     </div>
   )
 }
